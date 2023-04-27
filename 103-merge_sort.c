8d3ebdbceb733ff53bd5d8a02ed82823a4ce5297
Denollypaco <paschal4jesus@gmail.com>
2023-04-27 16:31:59 -0700

    Merge sort

---
+++ 103-merge_sort.c
@@ -0,0 +1,82 @@
+#include "sort.h"
+
+void merge_subarr(int *subarr, int *buff, size_t front, size_t mid,
+		size_t back);
+void merge_sort_recursive(int *subarr, int *buff, size_t front, size_t back);
+void merge_sort(int *array, size_t size);
+
+/**
+ * merge_subarr - This Sort a subarray of integers.
+ * @subarr: This is a subarray of an array of integers to sort.
+ * @buff: This is a buffer to store the sorted subarray.
+ * @front: This is the front index of the array.
+ * @mid: This is the middle index of the array.
+ * @back: This is the back index of the array.
+ */
+void merge_subarr(int *subarr, int *buff, size_t front, size_t mid,
+		size_t back)
+{
+	size_t i, j, k = 0;
+
+	printf("Merging...\n[left]: ");
+	print_array(subarr + front, mid - front);
+
+	printf("[right]: ");
+	print_array(subarr + mid, back - mid);
+
+	for (i = front, j = mid; i < mid && j < back; k++)
+		buff[k] = (subarr[i] < subarr[j]) ? subarr[i++] : subarr[j++];
+	for (; i < mid; i++)
+		buff[k++] = subarr[i];
+	for (; j < back; j++)
+		buff[k++] = subarr[j];
+	for (i = front, k = 0; i < back; i++)
+		subarr[i] = buff[k++];
+
+	printf("[Done]: ");
+	print_array(subarr + front, back - front);
+}
+
+/**
+ * merge_sort_recursive - This Implementa the merge sort algorithm through recursion.
+ * @subarr: This is a subarray of an array of integers to sort.
+ * @buff: This is a buffer to store the sorted result.
+ * @front: This the front index of the subarray.
+ * @back: This is the back index of the subarray.
+ */
+void merge_sort_recursive(int *subarr, int *buff, size_t front, size_t back)
+{
+	size_t mid;
+
+	if (back - front > 1)
+	{
+		mid = front + (back - front) / 2;
+		merge_sort_recursive(subarr, buff, front, mid);
+		merge_sort_recursive(subarr, buff, mid, back);
+		merge_subarr(subarr, buff, front, mid, back);
+	}
+}
+
+/**
+ * merge_sort - This Sorts an array of integers in ascending
+ *              order using the merge sort algorithm.
+ * @array: This is an array of integers.
+ * @size: This is the size of the array.
+ *
+ * Description: This Implements the top-down merge sort algorithm.
+ */
+void merge_sort(int *array, size_t size)
+{
+	int *buff;
+
+	if (array == NULL || size < 2)
+		return;
+
+	buff = malloc(sizeof(int) * size);
+	if (buff == NULL)
+		return;
+
+	merge_sort_recursive(array, buff, 0, size);
+
+	free(buff);
+}
